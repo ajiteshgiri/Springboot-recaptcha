abf7edcd3bb9f4154416e4212beed16b04dc8746
ajiteshgiri <ajiteshkumar4512@gmail.com>
2022-09-11 21:14:12 +0530

    first commit

---
+++ springboot-recaptcha/src/main/java/com/captcha/SpringbootRecaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.captcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRecaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRecaptchaApplication.class, args);
+	}
+
+}
